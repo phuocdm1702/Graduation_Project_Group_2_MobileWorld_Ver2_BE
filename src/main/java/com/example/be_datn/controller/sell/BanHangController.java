4959b788a8d9ad4c928db3f28114ebc56ab642ef
Huân Nè <Huannek2006@gmail.com>
2025-06-28 00:29:43 +0700

    fix lai crud imei va xoa hd cho

--- src/main/java/com/example/be_datn/controller/sell/BanHangController.java
+++ src/main/java/com/example/be_datn/controller/sell/BanHangController.java
@@ -9,6 +9,7 @@ import com.example.be_datn.entity.discount.PhieuGiamGia;
 import com.example.be_datn.entity.discount.PhieuGiamGiaCaNhan;
 import com.example.be_datn.entity.inventory.ChiTietGioHang;
 import com.example.be_datn.entity.order.HoaDon;
+import com.example.be_datn.entity.product.ChiTietSanPham;
 import com.example.be_datn.service.discount.PhieuGiamGiaCaNhanService;
 import com.example.be_datn.service.discount.PhieuGiamGiaService;
 import com.example.be_datn.service.sell.BanHangService;
@@ -47,8 +48,8 @@ public class BanHangController {
     }
 
     @DeleteMapping("/xoa-hd-cho/{idHD}")
-    public ResponseEntity<Void> huyHDCho(@PathVariable Integer idHD) throws Exception {
-        banHangService.huyHDCho(idHD);
+    public ResponseEntity<Void> huyHDCho(@PathVariable Integer idHD) {
+        banHangService.huyHD(idHD);
         return ResponseEntity.ok().build();
     }
 
@@ -138,6 +139,16 @@ public class BanHangController {
         try {
             Integer chiTietSanPhamId = banHangService.getChiTietSanPhamId(sanPhamId, mauSac, dungLuongRam, dungLuongBoNhoTrong);
             return ResponseEntity.ok(chiTietSanPhamId);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @GetMapping("/chi-tiet-san-pham/by-imei")
+    public ResponseEntity<ChiTietSanPham> getChiTietSanPhamByIMEI(@RequestParam("imei") String imei) {
+        try {
+            ChiTietSanPham chiTietSanPham = banHangService.getChiTietSanPhamByIMEI(imei);
+            return ResponseEntity.ok(chiTietSanPham);
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
--- src/main/java/com/example/be_datn/service/sell/BanHangService.java
+++ src/main/java/com/example/be_datn/service/sell/BanHangService.java
@@ -7,6 +7,7 @@ import com.example.be_datn.dto.sell.request.HoaDonDTO;
 import com.example.be_datn.dto.sell.response.ChiTietSanPhamGroupDTO;
 import com.example.be_datn.entity.discount.PhieuGiamGiaCaNhan;
 import com.example.be_datn.entity.order.HoaDon;
+import com.example.be_datn.entity.product.ChiTietSanPham;
 import org.springframework.data.domain.Page;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,7 +16,7 @@ import java.util.List;
 public interface BanHangService {
     List<HoaDon> getHDCho();
 
-    void huyHDCho(Integer idHD) throws Exception;
+    void huyHD(Integer idHD);
 
 
     @Transactional
@@ -37,6 +38,8 @@ public interface BanHangService {
     Page<ChiTietSanPhamGroupDTO> getAllCTSP(int page, int size, String keyword);
 
     List<String> getIMEIsBySanPhamIdAndAttributes(Integer sanPhamId, String mauSac, String dungLuongRam, String dungLuongBoNhoTrong);
+
+    ChiTietSanPham getChiTietSanPhamByIMEI(String imei);
 
     List<PhieuGiamGiaCaNhan> findByKhachHangId(Integer idKhachHang);
     Integer getChiTietSanPhamId(Integer sanPhamId, String mauSac, String dungLuongRam, String dungLuongBoNhoTrong);

b89042f63f55d7b2ada26684462ee3a228c03950
NgVietKhoa <khoa2006nguyen811@mail.com>
2025-07-08 15:18:46 +0700

    anh ctsp

--- src/main/java/com/example/be_datn/repository/product/ChiTietSanPhamRepository.java
+++ src/main/java/com/example/be_datn/repository/product/ChiTietSanPhamRepository.java
@@ -39,19 +39,6 @@ public interface ChiTietSanPhamRepository extends JpaRepository<ChiTietSanPham, 
             "GROUP BY sp.id, sp.tenSanPham, ms.mauSac, r.dungLuongRam, bnt.dungLuongBoNhoTrong")
     List<Object[]> findGroupedProductsBySanPhamId(@Param("sanPhamId") Integer sanPhamId);
 
-//    @Query("SELECT MIN(sp.ma) AS ma, sp.tenSanPham AS tenSanPham, ms.mauSac AS mauSac, r.dungLuongRam AS dungLuongRam, bnt.dungLuongBoNhoTrong AS dungLuongBoNhoTrong, " +
-//            "(SELECT COUNT(c2) FROM ChiTietSanPham c2 WHERE c2.idSanPham.id = sp.id AND c2.deleted = false AND NOT EXISTS (SELECT i FROM ImelDaBan i WHERE i.imel = c2.idImel.imel AND i.deleted = false)) AS soLuong, " +
-//            "MIN(c.giaBan) AS giaBan, sp.id AS idSanPham " +
-//            "FROM ChiTietSanPham c " +
-//            "JOIN c.idSanPham sp " +
-//            "LEFT JOIN c.idMauSac ms " +
-//            "LEFT JOIN c.idRam r " +
-//            "LEFT JOIN c.idBoNhoTrong bnt " +
-//            "WHERE (:sanPhamId IS NULL OR c.idSanPham.id = :sanPhamId) AND c.deleted = false " +
-//            "AND NOT EXISTS (SELECT i FROM ImelDaBan i WHERE i.imel = c.idImel.imel AND i.deleted = false) " +
-//            "GROUP BY sp.id, sp.tenSanPham, ms.mauSac, r.dungLuongRam, bnt.dungLuongBoNhoTrong")
-//    List<Object[]> findGroupedProductsBySanPhamId(@Param("sanPhamId") Integer sanPhamId);
-
     @Query("SELECT c.idImel.imel FROM ChiTietSanPham c " +
             "WHERE c.idSanPham.id = :sanPhamId AND c.deleted = false " +
             "AND c.idMauSac.mauSac = :mauSac AND c.idRam.dungLuongRam = :dungLuongRam AND c.idBoNhoTrong.dungLuongBoNhoTrong = :dungLuongBoNhoTrong " +
@@ -63,7 +50,6 @@ public interface ChiTietSanPhamRepository extends JpaRepository<ChiTietSanPham, 
 
     @Query("SELECT c FROM ChiTietSanPham c WHERE c.idSanPham.id = :sanPhamId AND c.idMauSac.mauSac = :mauSac AND c.idRam.dungLuongRam = :dungLuongRam AND c.idBoNhoTrong.dungLuongBoNhoTrong = :dungLuongBoNhoTrong AND c.deleted = false")
     Optional<ChiTietSanPham> findByIdSanPhamIdAndAttributes();
-
 
     @Query("SELECT c FROM ChiTietSanPham c WHERE c.idImel.imel = :imei AND c.deleted = false")
     Optional<ChiTietSanPham> findByIdImelImelAndDeletedFalse(@Param("imei") String imei);
@@ -81,7 +67,8 @@ public interface ChiTietSanPhamRepository extends JpaRepository<ChiTietSanPham, 
 
     @Query("SELECT c.idSanPham.tenSanPham AS tenSanPham, c.ma AS maSanPham, c.idImel.imel AS imei, " +
             "c.idMauSac.mauSac AS mauSac, c.idRam.dungLuongRam AS dungLuongRam, " +
-            "c.idBoNhoTrong.dungLuongBoNhoTrong AS dungLuongBoNhoTrong, c.giaBan AS donGia, c.deleted AS deleted " +
+            "c.idBoNhoTrong.dungLuongBoNhoTrong AS dungLuongBoNhoTrong, c.giaBan AS donGia, " +
+            "c.deleted AS deleted, c.idAnhSanPham.duongDan AS imageUrl " +
             "FROM ChiTietSanPham c " +
             "WHERE c.idSanPham.id = :idSanPham AND c.deleted = false")
     List<Object[]> findProductDetailsBySanPhamId(@Param("idSanPham") Integer idSanPham);

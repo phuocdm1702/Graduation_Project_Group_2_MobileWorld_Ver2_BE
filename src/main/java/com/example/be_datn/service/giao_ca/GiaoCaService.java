5ce9342e447e9aad6a2a3b623b3ca5843aa2f1dc
Huân Nè <Huannek2006@gmail.com>
2025-08-02 02:34:22 +0700

    lm giao ca 80%

--- src/main/java/com/example/be_datn/service/giao_ca/GiaoCaService.java
+++ src/main/java/com/example/be_datn/service/giao_ca/GiaoCaService.java
@@ -1,12 +1,18 @@
-
 package com.example.be_datn.service.giao_ca;
+
+import com.example.be_datn.entity.giao_ca.GiaoCa;
+import org.springframework.core.io.InputStreamResource;
 
 import java.io.ByteArrayInputStream;
 import java.math.BigDecimal;
 import java.util.Map;
+import java.util.Optional;
 
 public interface GiaoCaService {
-    Map<String, Object> startShift(Integer nhanVienId);
-    Map<String, Object> endShift(Integer nhanVienId, BigDecimal tienMatCuoiCa, BigDecimal tienMatBanDau);
+    GiaoCa startShift(Integer nhanVienId, BigDecimal tienMatBanDau);
+    GiaoCa endShift(Integer nhanVienId, BigDecimal tienMatCuoiCa);
     ByteArrayInputStream generateExcelReport(Map<String, Object> reportData);
+    Optional<GiaoCa> getActiveShift(Integer nhanVienId);
+    long getPendingOrdersCount();
+    Optional<BigDecimal> getLastEndedShiftCash();
 }
